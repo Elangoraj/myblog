d45d310b294af56e75d7fcc26ec4bcff10101a32
Aarif1430 <malikarif13@gmail.com>
2020-07-12 12:09:47 +0530

    changed layout of experiment to grid

--- src/pages/experiment.js
+++ src/pages/experiment.js
@@ -8,44 +8,58 @@ import styled from "styled-components"
 import Image from "gatsby-image"
 
 const BannerImage = styled(Image)`
-  border-radius: 10px;
-  width: 30rem;
+  width: 20rem;
   margin-top: 20px;
 `
 
 const ExperimentPage = ({ data }) => (
   <Layout>
     <h3> My Experiment </h3>
-    {data.allMarkdownRemark.edges.map(post => (
-      <div key={post.node.id}>
-        <h3>
-          <Link className="postTitle" to={post.node.frontmatter.path}>
-            {post.node.frontmatter.title}
+    <div
+      className="experiment-grid"
+      style={{
+        gridTemplateColumns: `repeat(2,1fr)`,
+        gridColumnGap: `25px`,
+        display: `grid`,
+      }}
+    >
+      {" "}
+      {data.allMarkdownRemark.edges.map(post => (
+        <div
+          key={post.node.id}
+          style={{ position: `relative`, overflow: `hidden` }}
+        >
+          <h3>
+            <Link className="postTitle" to={post.node.frontmatter.path}>
+              {post.node.frontmatter.title}
+            </Link>
+          </h3>
+          <small style={{ fontSize: "1rem", fontStyle: "italic" }}>
+            {" "}
+            {post.node.frontmatter.date}
+            {"  "}
+            <span style={{ color: "#DAA520", fontWeight: "Bold" }}>
+              {post.node.frontmatter.category}
+            </span>
+          </small>
+          <Link to={post.node.frontmatter.path}>
+            <BannerImage
+              fluid={post.node.frontmatter.featured.childImageSharp.fluid}
+              alt="Banner Image"
+            />
           </Link>
-        </h3>
-        <small style={{ fontSize: "1rem", fontStyle: "italic" }}>
-          {" "}
-          {post.node.frontmatter.date}
-          {"  "}
-          <span style={{ color: "#DAA520", fontWeight: "Bold" }}>
-            {post.node.frontmatter.category}
-          </span>
-        </small>
-        <Link to={post.node.frontmatter.path}>
-          <BannerImage
-            fluid={post.node.frontmatter.featured.childImageSharp.fluid}
-            alt="Banner Image"
-          />
-        </Link>
-        <p style={{ fontSize: "1rem", marginBottom: "0", marginTop: "0.7rem" }}>
-          {post.node.frontmatter.desc}
-        </p>
-        <br></br>
-        <Link to={post.node.frontmatter.path}>Read More</Link>
-        <br></br>
-        <hr></hr>
-      </div>
-    ))}
+          <p
+            style={{ fontSize: "1rem", marginBottom: "0", marginTop: "0.7rem" }}
+          >
+            {post.node.frontmatter.desc}
+          </p>
+          <br></br>
+          <Link to={post.node.frontmatter.path}>Read More</Link>
+          <br></br>
+          <hr></hr>
+        </div>
+      ))}
+    </div>
   </Layout>
 )
 
